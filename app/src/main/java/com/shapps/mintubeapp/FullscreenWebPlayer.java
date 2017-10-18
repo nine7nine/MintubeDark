2ca325302de2a8f7f549a0b6c702a8a43aeab1af
ninez <johnstonljordan@gmail.com>
2017-10-17 21:11:18 -0400

    Add Immersive Mode in FullscreenWebPlayer Round 2 - make sticky [still need to fix the horizontal part - even if navbar is hidden, there is still empty space....ugh

--- app/src/main/java/com/shapps/mintubeapp/FullscreenWebPlayer.java
+++ app/src/main/java/com/shapps/mintubeapp/FullscreenWebPlayer.java
@@ -50,7 +50,8 @@ public class FullscreenWebPlayer extends Activity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
         );
     }
 
