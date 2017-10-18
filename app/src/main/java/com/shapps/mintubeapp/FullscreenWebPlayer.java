d0b96c6ca20f1ac3ff5b663ba39a61beb35b3531
ninez <johnstonljordan@gmail.com>
2017-10-17 20:28:23 -0400

    Add Immersive Mode in FullscreenWebPlayer

--- app/src/main/java/com/shapps/mintubeapp/FullscreenWebPlayer.java
+++ app/src/main/java/com/shapps/mintubeapp/FullscreenWebPlayer.java
@@ -2,6 +2,7 @@ package com.shapps.mintubeapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.webkit.WebView;
@@ -39,6 +40,20 @@ public class FullscreenWebPlayer extends Activity {
         WebPlayer.loadScript(JavaScript.playVideoScript());
 
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
+    }
+
     @Override
     public void onBackPressed() {
         if(active){
