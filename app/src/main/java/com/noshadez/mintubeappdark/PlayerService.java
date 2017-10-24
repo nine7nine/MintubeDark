24bf07b69983049511dbe35a0065f9a25c1ae607
ninez <johnstonljordan@gmail.com>
2017-10-24 17:28:58 -0400

    Lint: misc fixes, mostly to xml

--- app/src/main/java/com/noshadez/mintubeappdark/PlayerService.java
+++ app/src/main/java/com/noshadez/mintubeappdark/PlayerService.java
@@ -27,7 +27,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -35,7 +34,6 @@ import android.widget.RemoteViews;
 
 import com.noshadez.mintubeappdark.AsyncTask.ImageLoadTask;
 import com.noshadez.mintubeappdark.AsyncTask.LoadDetailsTask;
-import com.noshadez.mintubeappdark.CustomViews.CircularImageView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
